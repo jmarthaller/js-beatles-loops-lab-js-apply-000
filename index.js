0ec021e576ef0480276efeccce5bf3d7d95e0def
Jacob Marthaller <marthaller.jr@gmail.com>
2020-09-25 19:50:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicianArray, instrumentArray) {
   let result = [];
   
   for (let i = 0; i < musicianArray.length; i++) {
-    result += `${musicianArray[i]} plays ${instrumentArray[i]}`;
+    result += `${musicianArray[i]} plays ${instrumentArray}`;
   }
   return result.split(' ');
 }
